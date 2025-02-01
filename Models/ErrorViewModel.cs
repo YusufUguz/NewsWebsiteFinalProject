10a7dedd4ea8d7151f78d1dba817ba8f33677da3
Yusuf Uguz <yusuf_uguz60@hotmail.com>
2025-02-01 14:39:32 +0300

    Project started,general layout created by creating header and footer.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NewsWebsiteFinalProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
